c3b51594c4c655640ee685327dad27f6a70b0494
Sam Billingham <sambillie.sb@gmail.com>
2018-06-22 15:03:42 +0100

    add - plugin base

---
+++ finallycomments-wp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package  FinallyCommentsWP
+ */
+/*
+Plugin Name: Finally Comments WP
+Plugin URI: https://finallycomments.com/plugin
+Description: Comments Plugin for working with the STEEM blockchain
+Version: 0.1.0
+Author: Sam Billingham
+Author URI: https://sambillingham.com
+License: GPLv2 or later
+Text Domain: finallycomments-wp
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
